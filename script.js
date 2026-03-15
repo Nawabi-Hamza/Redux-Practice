5b36f3a4e8bb35f5147c8291af87cd35b8e12a3d
Hamza-Nawabi <hamza.nawabi119@gmail.com>
2026-03-15 05:27:38 +0430

    add functions for more preformant

--- script.js
+++ script.js
@@ -10,13 +10,26 @@ const store = {
             this.state.cart.push(action.payload)
         }
         else if(action.type == "REMOVE-FROM-CART"){
-            this.state.cart.pop()
+            const filterCart = this.state.cart.filter( i => i !== action.payload)
+            this.state.cart = filterCart
         }
     }
 }
 
-store.dispatch({ type: "ADD-TO-CART", payload: "Banana" })
+function addToCart(product){
+    return { type: "ADD-TO-CART", payload: product }
+}
+function removeFromCart(product){
+    return { type: "REMOVE-FROM-CART", payload: product }
+}
 
-store.dispatch({ type: "REMOVE-FROM-CART" })
+store.dispatch(addToCart("Apple"))
+store.dispatch(addToCart("Banana"))
+store.dispatch(addToCart("Strawberry"))
+store.dispatch(addToCart("Pear"))
+
+store.dispatch(removeFromCart("Apple"))
+store.dispatch(removeFromCart("Pear"))
+
 
 console.log(store.getState())
